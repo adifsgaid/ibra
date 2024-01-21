a3a1c4072e524eed6bc72e56381b74b65386f6c0
Adif Sgaid <69080239+adifsgaid@users.noreply.github.com>
2024-01-21 12:49:26 +0100

    Ibork landing page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -28,30 +28,13 @@ const Header = ({ t }: any) => {
   };
 
   const MenuItem = () => {
-    const scrollTo = (id: string) => {
-      const element = document.getElementById(id) as HTMLDivElement;
-      element.scrollIntoView({
-        behavior: "smooth",
-      });
-      setVisibility(false);
-    };
     return (
       <>
-        <CustomNavLinkSmall onClick={() => scrollTo("about")}>
-          <Span>{t("About")}</Span>
-        </CustomNavLinkSmall>
-        <CustomNavLinkSmall onClick={() => scrollTo("mission")}>
-          <Span>{t("Mission")}</Span>
-        </CustomNavLinkSmall>
-        <CustomNavLinkSmall onClick={() => scrollTo("product")}>
-          <Span>{t("Product")}</Span>
-        </CustomNavLinkSmall>
-        <CustomNavLinkSmall
-          style={{ width: "180px" }}
-          onClick={() => scrollTo("contact")}
-        >
+        <CustomNavLinkSmall style={{ width: "180px" }}>
           <Span>
-            <Button>{t("Contact")}</Button>
+            <a href="mailto:iborkimpianti@gmail.com">
+              <Button>{t("Contattaci")}</Button>
+            </a>
           </Span>
         </CustomNavLinkSmall>
       </>
@@ -63,7 +46,7 @@ const Header = ({ t }: any) => {
       <Container>
         <Row justify="space-between">
           <LogoContainer to="/" aria-label="homepage">
-            <SvgIcon src="logo.svg" width="101px" height="64px" />
+            <SvgIcon src="logo.svg" width="200px" height="250px" />
           </LogoContainer>
           <NotHidden>
             <MenuItem />
